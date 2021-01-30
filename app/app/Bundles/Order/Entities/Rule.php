a1354875175a7da2d640e234ecaf33af676d48a6
Majid Akbari <majid.akbari@neocard.fi>
2021-01-30 17:24:12 +0200

    create model and migration for rule entity

---
+++ app/app/Bundles/Order/Entities/Rule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Bundles\Order\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rule extends Model
+{
+    use HasFactory;
+}
---
+++ app/database/migrations/2021_01_30_152239_create_rules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rules', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rules');
+    }
+}
