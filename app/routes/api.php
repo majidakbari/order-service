6db8999176e2c0fad6d7d103ff6d7dc148e4a986
Majid Akbari <majid.akbari@neocard.fi>
2021-01-31 16:06:59 +0200

    add swagger

--- app/routes/api.php
+++ app/routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/order')->namespace('Order\Controllers')->as('order.')->group(function () {
-        Route::post('/', 'GetOrderPriceController')->name('price.get');
+        Route::post('/price', 'GetOrderPriceController')->name('price.get');
 });
